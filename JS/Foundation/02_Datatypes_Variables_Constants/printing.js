1fbd081c3e3b7f3265d2435178d0a69747fe1ae9
Suraj Kumar <surajxcode@icloud.com>
2025-07-07 20:10:33 +0530

    JSFoundation: Introduction, Datatypes, Variables & Constants
    Signed-off-by: Suraj Kumar <surajxcode@icloud.com>

---
+++ JS/Foundation/02_Datatypes_Variables_Constants/printing.js
@@ -0,0 +1,4 @@
+console.log("Hello Suraj");
+process.stdout.write("Other ways of printing"); 
+process.stdout.write("Chai Chai"); // it's gonna print in same line.
+console.table({city: "Delhi"}); // print like table 
