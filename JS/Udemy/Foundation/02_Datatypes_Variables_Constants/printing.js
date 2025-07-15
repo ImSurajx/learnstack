c3755a7300184538e20e6371fd88741f75856e7d
Suraj Kumar <surajxcode@icloud.com>
2025-07-15 22:10:54 +0530

    JS: Remove Udemy JS Foundation moving udemy js course to different js course i found this course on youtube the tutor is same but here he teach us in depth
    Signed-off-by: Suraj Kumar <surajxcode@icloud.com>

--- JS/Udemy/Foundation/02_Datatypes_Variables_Constants/printing.js
+++
@@ -1,4 +0,0 @@
-console.log("Hello Suraj");
-process.stdout.write("Other ways of printing"); 
-process.stdout.write("Chai Chai"); // it's gonna print in same line.
-console.table({city: "Delhi"}); // print like table 
